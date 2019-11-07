11967f24b0d04cc02afcc051207bf1beb853eff7
Aral Balkan <mail@ar.al>
2019-11-07 20:30:05 +0000

    Evolve into initial @small-tech/http module

---
+++ example/index.js
@@ -0,0 +1,30 @@
+const Express = require('express')
+const https = require('..')
+const hostname = require('@small-tech/cross-platform-hostname')
+
+//
+// Create and configure a basic “Hello, world!” Express app.
+//
+const app = Express()
+
+app.get('/', (request, response) => {
+  response.writeHeader(200, {'Content-Type': 'text/html'})
+  response.end('<!doctype html><html lang=\'en\'><head><meta charset=\'utf-8\'/><title>Hello, world!</title><style>body{background-color: white; font-family: sans-serif;}</style></head><body><h1>Hello, world!</h1></body></html>')
+})
+
+//
+// Create an HTTPS server with
+//
+
+const options = {
+  server: 'staging',
+  configDir: `./example/config/${hostname}`,
+  approvedDomains: [hostname],
+  debug: true
+}
+
+const server = https.createServer(options, app)
+
+server.listen(443, () => {
+  console.log(' 🎉 Server running on port 443.')
+})
