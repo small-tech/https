3c9ab9752efb3df7edec3805a4e93bbb189d0eea
Aral Balkan <mail@ar.al>
2019-11-09 13:53:49 +0000

    Simplify example

--- example/index.js
+++ example/index.js
@@ -4,7 +4,7 @@ const https = require('..')
 function html(message) {
   return `<!doctype html><html lang='en'><head><meta charset='utf-8'/><title>Hello, world!</title><style>body{background-color: white; font-family: sans-serif;}</style></head><body><h1>${message}</h1></body></html>`
 }
-const contentTypeHTML = {'Content-Type': 'text/html'}
+const headers = {'Content-Type': 'text/html'}
 
 let options = {}
 
@@ -18,13 +18,8 @@ let options = {}
 
 // Create HTTPS server at https://localhost with locally-trusted certificates.
 const server = https.createServer(options, (request, response) => {
-  if (request.method !== 'GET') {
-    response.writeHead(404, contentTypeHTML)
-    response.end(html('Not found.'))
-    return
-  }
   // Respond to all routes with the same page.
-  response.writeHead(200, contentTypeHTML)
+  response.writeHead(200, headers)
   response.end(html('Hello, world!'))
 })
 
