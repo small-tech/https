9675135a97415090f05d016f2057d6f3db08449e
Aral Balkan <mail@ar.al>
2020-04-15 18:30:11 +0100

    Remove Pebble server option

--- index.js
+++ index.js
@@ -1,25 +1,14 @@
-const os = require('os')
-const path = require('path')
-const https = require('https')
-const AutoEncrypt = require('@small-tech/auto-encrypt')
+const os                   = require('os')
+const path                 = require('path')
+const https                = require('https')
+const log                  = require('./lib/util/log')
+const AutoEncrypt          = require('@small-tech/auto-encrypt')
 const AutoEncryptLocalhost = require('@small-tech/auto-encrypt-localhost')
 
-function log(...args) {
-  if (process.env.QUIET) {
-    return
-  }
-  console.log(...args)
-}
+const AUTO_ENCRYPT_STAGING_SERVER_TYPE = 1
 
 // Only modify this instance of the https module with our own createServer method.
 const smallTechHttps = Object.assign({}, https)
-
-// Enumeration. The server types expected by AutoEncrypt. Default is production.
-AutoEncryptServerType = {
-  PRODUCTION: 0,
-  STAGING: 1,
-  PEBBLE: 2,
-}
 
 smallTechHttps.createServer = function (options, listener) {
     // The first parameter is optional. If omitted, listener should be passed as the first argument.
@@ -33,10 +22,8 @@ smallTechHttps.createServer = function (options, listener) {
 
     options.settingsPath = options.settingsPath ? path.join(path.resolve(options.settingsPath), serverScope) : path.join(defaultSettingsPath, serverScope)
 
-    if (options.staging) { options.serverType = AutoEncryptServerType.STAGING }
-    if (options.pebble)  { options.serverType = AutoEncryptServerType.PEBBLE  }
+    if (options.staging) { options.serverType = AUTO_ENCRYPT_STAGING_SERVER_TYPE }
     delete options.staging
-    delete options.pebble
 
     const logMessage = {
       local: 'at localhost with locally-trusted certificates',
@@ -50,6 +37,13 @@ smallTechHttps.createServer = function (options, listener) {
 
     log(`   🔒    ❨@small-tech/https❩ Creating server ${logMessage[serverScope]}.`)
     const server = autoEncryptScope[serverScope].https.createServer(options, listener)
+
+    if (serverScope === 'global') {
+      server.on('close', () => {
+        // Allow AutoEncrypt to perform clean-up (e.g., remove interval timer for renewal check, etc.)
+        AutoEncrypt.shutdown()
+      })
+    }
 
     log('   🔒    ❨@small-tech/https❩ Created HTTPS server.')
     return server
